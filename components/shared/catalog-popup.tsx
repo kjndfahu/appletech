46d0e3a16d55836106aa356667d398fd290517ba
kjndfahu <denispoda59@gmail.com>
2024-09-25 23:23:32 +0300

    first commit

---
+++ components/shared/catalog-popup.tsx
@@ -0,0 +1,11 @@
+interface Props{
+    className?: string;
+}
+
+export const CatalogPopup:React.FC<Props> =({className}) => {
+    return (
+        <div className="flex">
+            <div className="flex bg-[#0071E4]"></div>
+        </div>
+    )
+}
